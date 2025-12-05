7eeb7c6cf3be4918d392a4f236dcb1bd1e33475b
RojoEmiliano <rojoemiliano515@gmail.com>
2025-12-05 16:14:30 -0300

    Navbar responsive + cambios generales

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,11 +2,15 @@ import { Box } from '@mui/material';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import CartSidebar from '../cart/CartSidebar';
+import './navbar.scss'
+import ResponsiveNavbar from './ResponsiveNavbar';
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 const Layout = ({ children }) => {
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-      <Navbar />
+      <ResponsiveNavbar />
       <Box component="main" sx={{ flexGrow: 1 }}>
         {children}
       </Box>
